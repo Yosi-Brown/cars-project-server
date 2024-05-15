2f32212870a7f6a48b919709d846048d34a6df31
Yosi-Brown <www.yosi@gmail.com>
2024-05-15 09:29:15 +0300

    15/05/24

--- router/productRouter.js
+++ router/productRouter.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { addProduct } = require("../controller/productController");
 
-router.post("/product", addProduct);
+router.post("/add", addProduct);
 
 module.exports = router;
