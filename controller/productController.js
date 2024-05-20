66e57154b750b1bd4e681ffe4590f93b6760f3c4
Yosi-Brown <www.yosi@gmail.com>
2024-05-20 10:00:11 +0300

    19/05/24

--- controller/productController.js
+++ controller/productController.js
@@ -1,15 +1,24 @@
 const mongoose = require("mongoose");
 const ProductModel = require("../model/productModel");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 
 module.exports = {
   addProduct: async (req, res) => {
     try {
-      // console.log(req)
+      if (req.file) {
+        const uploadImage = await cloudinary.uploader.upload(req.file.path)
+        console.log(uploadImage.url);
+        req.body.image = uploadImage.url
+      }
+
       const car = req.body;
-      const imgCar = car.image
-      console.log(imgCar);
-      // const singleCar = ProductModel(car);
-      // await singleCar.save();
+      console.log(car);
       return res.status(200).json({
         message: "successfully to push products",
         success: true,
--- router/productRouter.js
+++ router/productRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { addProduct } = require("../controller/productController");
+const upload = require("../middleware/uploadFiles");
 
-router.post("/add", addProduct);
+router.post("/add", upload.single('image'), addProduct);
 
 module.exports = router;
