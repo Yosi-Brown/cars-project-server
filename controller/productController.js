64c8a75d6e820a7cd3bc683f5a101d3dbba8be39
elchanan <you@example.com>
2024-05-23 11:03:58 +0300

    23/5 elchanan

--- controller/productController.js
+++ controller/productController.js
@@ -47,5 +47,47 @@ module.exports = {
         success: false,
       })
     }
-  }
-};
+  },
+  deleteProduct: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deletedProduct = await ProductModel.findByIdAndDelete(id);
+      // console.log(deletedProduct)
+
+      if (!deletedProduct) {throw new Error("Product not found")}
+
+
+      return res.status(200).json({
+        message: "Product deleted successfully",
+        success: true,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: "Failed to delete product",
+        error: error.message,
+        success: false,
+      });
+    }
+  },
+  updateProduct: async (req, res) =>{
+    try {
+      const { id } = req.params;
+      const updatedData = req.body;
+    const updatedProduct = await ProductModel.findByIdAndUpdate(id, updatedData, { new: true });
+      if (!updatedProduct) throw new Error('Product not found')
+  
+      return res.status(200).json({
+        success: true,
+        message: 'Product updated successfully',
+        product: updatedProduct
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: 'Failed to update product',
+        error: error.message
+      });
+    }
+  }}     
+
+;
